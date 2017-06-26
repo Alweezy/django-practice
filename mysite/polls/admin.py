5d4fdfce418517a017529cf6f1881dd623710982
Alweezy <alvinmutisya@gmail.com>
2017-06-26 10:06:29 +0300

    Edit field sets configuration

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
@@ -11,7 +11,8 @@ class ChoiceInLine(admin.StackedInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
