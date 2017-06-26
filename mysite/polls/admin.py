180cf003bae35ce8ad1f2ae7dc49b87f4ba514a3
Alweezy <alvinmutisya@gmail.com>
2017-06-26 10:00:51 +0300

    Add choice fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
